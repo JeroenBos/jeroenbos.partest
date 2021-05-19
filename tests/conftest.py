93ca70468662e7c7ed592f431aba9a2608feaad7
Jeroen Bos <jeroen@deepinsight.io>
2021-05-20 00:14:34 +0200

    Simplify hooking typeguard

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 # this file `conftest.py` gets called by pytest automatically, before anything else
 import logging
+import os
 import sys
 from typeguard import importhook
 from tests import SrcTypeguardFinder
@@ -7,7 +8,9 @@ from tests import SrcTypeguardFinder
 logging.getLogger().setLevel("DEBUG")
 
 # prevent all imports in tests/ from requiring the prefix "src."
-sys.path.insert(1, "./src")
+EXTRA_PATH = os.getcwd() + "/src"
+sys.path.insert(1, EXTRA_PATH)
 
 # apply runtime typechecking to "./src"
+SrcTypeguardFinder.path_prefixes.append(EXTRA_PATH)
 importhook.install_import_hook([], cls=SrcTypeguardFinder)
