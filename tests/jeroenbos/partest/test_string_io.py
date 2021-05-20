ea811f5fb74a0905c07b18f6065c3114b75cd2e7
Jeroen Bos <jeroen@deepinsight.io>
2021-05-20 20:11:34 +0200

    Fix bug I don't understand
    super().__eq__(o) returns true even though id(o) != id(self)

--- tests/jeroenbos/partest/test_string_io.py
+++ tests/jeroenbos/partest/test_string_io.py
@@ -13,11 +13,9 @@ class TestStringIO(StringIO):
         return value in str(self)
 
     def __eq__(self, o: object) -> bool:
-        if super().__eq__(o):
-            return True
         if isinstance(o, str):
             return o == str(self)
-        return False
+        return super().__eq__(o)
 
     def __str__(self) -> str:
         return self.getvalue()
